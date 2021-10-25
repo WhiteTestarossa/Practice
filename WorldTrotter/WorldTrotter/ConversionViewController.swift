ae6ce05775ab81bc7665dd4d5b317c3cfc8ef241
Daniel Belokursky <daniel.belokursky@gmail.com>
2021-10-25 23:08:29 +0300

    feat: add map type changing control and layout

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -8,6 +8,69 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    //MARK: - Public Properties
+    var fahrenheitNumberLabel: UILabel!
+    var dgsFahrenheitLabel: UILabel!
+    var isRlyLabel: UILabel!
+    var celciusNumberLabel: UILabel!
+    var dgsCelciusLabel: UILabel!
+    
+    // MARK: - LoadView()
+    override func loadView() {
+        let backgroundView = UIView()
+        self.view = backgroundView
+        self.view.backgroundColor = UIColor(red: CGFloat(245)/255, green: CGFloat(244)/255, blue: CGFloat(241)/255, alpha: 1.0)
+        let labelsColor = UIColor(red: CGFloat(225)/255, green: CGFloat(88)/255, blue: CGFloat(41)/255, alpha: 1.0)
+        
+        fahrenheitNumberLabel = UILabel()
+        fahrenheitNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(fahrenheitNumberLabel)
+        fahrenheitNumberLabel.text = "212"
+        fahrenheitNumberLabel.textColor = labelsColor
+        fahrenheitNumberLabel.font = .systemFont(ofSize: CGFloat(70))
+        
+        dgsFahrenheitLabel = UILabel()
+        dgsFahrenheitLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(dgsFahrenheitLabel)
+        dgsFahrenheitLabel.text = "degrees Fahrenheit"
+        dgsFahrenheitLabel.textColor = labelsColor
+        dgsFahrenheitLabel.font = .systemFont(ofSize: CGFloat(36))
+        
+        isRlyLabel = UILabel()
+        isRlyLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(isRlyLabel)
+        isRlyLabel.text = "is really"
+        isRlyLabel.font = .systemFont(ofSize: CGFloat(36))
+        
+        celciusNumberLabel = UILabel()
+        celciusNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(celciusNumberLabel)
+        celciusNumberLabel.text = "100"
+        celciusNumberLabel.textColor = labelsColor
+        celciusNumberLabel.font = .systemFont(ofSize: CGFloat(70))
+        
+        dgsCelciusLabel = UILabel()
+        dgsCelciusLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(dgsCelciusLabel)
+        dgsCelciusLabel.text = "degrees Celcius"
+        dgsCelciusLabel.textColor = labelsColor
+        dgsCelciusLabel.font = .systemFont(ofSize: CGFloat(36))
+        
+        NSLayoutConstraint.activate([
+            fahrenheitNumberLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8.0),
+            fahrenheitNumberLabel.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            dgsFahrenheitLabel.topAnchor.constraint(equalTo: fahrenheitNumberLabel.bottomAnchor, constant: 8.0),
+            dgsFahrenheitLabel.centerXAnchor.constraint(equalTo: fahrenheitNumberLabel.centerXAnchor),
+            isRlyLabel.topAnchor.constraint(equalTo: dgsFahrenheitLabel.bottomAnchor, constant: 8.0),
+            isRlyLabel.centerXAnchor.constraint(equalTo: fahrenheitNumberLabel.centerXAnchor),
+            celciusNumberLabel.topAnchor.constraint(equalTo: isRlyLabel.bottomAnchor, constant: 8.0),
+            celciusNumberLabel.centerXAnchor.constraint(equalTo: fahrenheitNumberLabel.centerXAnchor),
+            dgsCelciusLabel.topAnchor.constraint(equalTo: celciusNumberLabel.bottomAnchor, constant: 8.0),
+            dgsCelciusLabel.centerXAnchor.constraint(equalTo: fahrenheitNumberLabel.centerXAnchor)
+        ])
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -12,9 +12,42 @@ class MapViewController: UIViewController {
     
     var mapView: MKMapView!
     
+    //MARK: - LoadView()
     override func loadView() {
         mapView = MKMapView()
         self.view = mapView
+        
+        //MARK: SegmentedControl
+        let segmentedControl = UISegmentedControl(items: ["Standart", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = .systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        self.view.addSubview(segmentedControl)
+        
+        //MARK: Constraints for SC
+        let margins = self.view.layoutMarginsGuide
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 8.0)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
+    
+    //MARK: - Functions
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
     }
     
 }
